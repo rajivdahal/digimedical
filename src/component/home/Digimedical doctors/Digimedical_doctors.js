a3bf7735e3abbee93d151a6604563d465de7feba
Bibek <bibekneupane51@gmail.com>
2022-01-21 17:02:38 +0545

    doc_appointform

--- src/component/home/Digimedical doctors/Digimedical_doctors.js
+++ src/component/home/Digimedical doctors/Digimedical_doctors.js
@@ -31,56 +31,238 @@ function Digimedical_doctors() {
           </div>
           <div className="doc_appoint_main">
             <div className="digidoctor_apoint_card">
-              <div className="digidoctor_apoint_card1">
-                <div className="digidoc_card_img">
-                  <img
-                    src={doctor1}
-                    alt=""
-                    style={{
-                      height: "140px",
-                      width: "140px",
-                      borderRadius: "50%",
-                    }}
-                  />
-                </div>
-                <div className="digidoctor_about_desc">
-                  <div className="doc_about_desc_head">
-                    <p id="doc_name_card">Rameshwor Shrestha Acharya</p>
-                    <p id="doc_edu_brief">Mbbs,MD</p>
+              <div className="digidoc_card1">
+                <div className="digi_first_card_content">
+                  <div className="digidoc_card_img">
+                    <img
+                      src={doctor1}
+                      alt=""
+                      style={{
+                        height: "140px",
+                        width: "140px",
+                        borderRadius: "50%",
+                      }}
+                    />
                   </div>
-
-                  <p id="digidoc_exp"> Gynaecologist & Obstetrician </p>
+                  <div className="digidoc_about_desc">
+                    <div className="doc_about_desc_head">
+                      <p id="doc_name_card">Hello Doc</p>
+                      <p id="doc_edu_brief">Doc Edu</p>
+                    </div>
+                    <div>
+                      <p>
+                        <b>Spe</b>
+                      </p>
+                      <p>Desc</p>
+                    </div>
+                  </div>
+                  <div className="digidoc_card_but">
+                    {" "}
+                    <button id="digidoc_card_but">Book an appointment</button>
+                  </div>
                 </div>
 
-                <div className="digidoctor_card_but">
-                  {" "}
-                  <button id="digidoctor_card_but">Book an appointment</button>
+                <div className="form_digi_doc">
+                  <div className="form_digidoc">
+                    <div className="digidoc_appoin_form1">
+                      <p>First Name</p>
+                      <input
+                        type="text"
+                        placeholder="Enter First Name"
+                        name="firstName"
+                        id="firstName"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Middle Name</p>
+                      <input
+                        type="text"
+                        placeholder="Enter Middle Name"
+                        name="middleName"
+                        id="middleName"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Last Name</p>
+                      <input
+                        type="text"
+                        placeholder="Enter Last Name"
+                        name="lastName"
+                        id="lastName"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Email Address</p>
+                      <input
+                        type="text"
+                        placeholder="Enter Email Address"
+                        name="email"
+                        id="email"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Phone No.</p>
+                      <input
+                        type="text"
+                        placeholder="Enter Phone no."
+                        name="mobileNumber"
+                        id="mobileNumber"
+                      />
+                    </div>
+
+                    <div class="digidoc_appoin_form1">
+                      <p>Appointment Date</p>
+
+                      <input
+                        type="date"
+                        name="appointmentDate"
+                        id="appointmentDate"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Appointment Time</p>
+                      <input
+                        type="time"
+                        name="appointmentTime"
+                        id="appointmentTime"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Select Service Type</p>
+                      <select name="cars" id="cars">
+                        <optgroup label="Swedish Cars">
+                          <option value="volvo">Volvo</option>
+                          <option value="saab">Saab</option>
+                        </optgroup>
+                        <optgroup label="German Cars">
+                          <option value="mercedes">Mercedes</option>
+                          <option value="audi">Audi</option>
+                        </optgroup>
+                      </select>
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <button type="submit" className="submit-buttons">
+                        Submit
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="digidoctor_apoint_card1">
-                <div className="digidoc_card_img">
-                  <img
-                    src={doctor1}
-                    alt=""
-                    style={{
-                      height: "140px",
-                      width: "140px",
-                      borderRadius: "50%",
-                    }}
-                  />
-                </div>
-                <div className="digidoctor_about_desc">
-                  <div className="doc_about_desc_head">
-                    <p id="doc_name_card">Rameshwor Shrestha Acharya</p>
-                    <p id="doc_edu_brief">Mbbs,MD</p>
+              <div className="digidoc_card1">
+                <div className="digi_first_card_content">
+                  <div className="digidoc_card_img">
+                    <img
+                      src={doctor1}
+                      alt=""
+                      style={{
+                        height: "140px",
+                        width: "140px",
+                        borderRadius: "50%",
+                      }}
+                    />
                   </div>
-
-                  <p id="digidoc_exp"> Gynaecologist & Obstetrician </p>
+                  <div className="digidoc_about_desc">
+                    <div className="doc_about_desc_head">
+                      <p id="doc_name_card">Hello Doc</p>
+                      <p id="doc_edu_brief">Doc Edu</p>
+                    </div>
+                    <div>
+                      <p>
+                        <b>Spe</b>
+                      </p>
+                      <p>Desc</p>
+                    </div>
+                  </div>
+                  <div className="digidoc_card_but">
+                    {" "}
+                    <button id="digidoc_card_but">Book an appointment</button>
+                  </div>
                 </div>
 
-                <div className="digidoctor_card_but">
-                  {" "}
-                  <button id="digidoctor_card_but">Book an appointment</button>
+                <div className="form_digi_doc">
+                  <div className="form_digidoc">
+                    <div className="digidoc_appoin_form1">
+                      <p>First Name</p>
+                      <input
+                        type="text"
+                        placeholder="Enter First Name"
+                        name="firstName"
+                        id="firstName"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Middle Name</p>
+                      <input
+                        type="text"
+                        placeholder="Enter Middle Name"
+                        name="middleName"
+                        id="middleName"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Last Name</p>
+                      <input
+                        type="text"
+                        placeholder="Enter Last Name"
+                        name="lastName"
+                        id="lastName"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Email Address</p>
+                      <input
+                        type="text"
+                        placeholder="Enter Email Address"
+                        name="email"
+                        id="email"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Phone No.</p>
+                      <input
+                        type="text"
+                        placeholder="Enter Phone no."
+                        name="mobileNumber"
+                        id="mobileNumber"
+                      />
+                    </div>
+
+                    <div class="digidoc_appoin_form1">
+                      <p>Appointment Date</p>
+
+                      <input
+                        type="date"
+                        name="appointmentDate"
+                        id="appointmentDate"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Appointment Time</p>
+                      <input
+                        type="time"
+                        name="appointmentTime"
+                        id="appointmentTime"
+                      />
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <p>Select Service Type</p>
+                      <select name="cars" id="cars">
+                        <optgroup label="Swedish Cars">
+                          <option value="volvo">Volvo</option>
+                          <option value="saab">Saab</option>
+                        </optgroup>
+                        <optgroup label="German Cars">
+                          <option value="mercedes">Mercedes</option>
+                          <option value="audi">Audi</option>
+                        </optgroup>
+                      </select>
+                    </div>
+                    <div class="digidoc_appoin_form1">
+                      <button type="submit" className="submit-buttons">
+                        Submit
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="digidoctor_apoint_card1">
