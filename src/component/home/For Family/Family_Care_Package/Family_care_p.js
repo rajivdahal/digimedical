87160a4fa39b1823022d3152a049b4baa5461434
Bibek <bibekneupane51@gmail.com>
2022-01-30 12:37:06 +0545

    design fcp solve

--- src/component/home/For Family/Family_Care_Package/Family_care_p.js
+++ src/component/home/For Family/Family_Care_Package/Family_care_p.js
@@ -2,163 +2,162 @@ import React from "react";
 import "./family_care_p.css";
 function family_care_p() {
   return (
-    <div>
-      <div className="package-page">
-        <div class="top-container">
-          <div class="up">
-            <a href="url" id="healthpackages">
-              Health Packages
-            </a>
-            <img src="Vector.png" className="arrow" />
-            <span id="familyhealthpackages">Family health packages</span>
-          </div>
-          <div className="down">
-            <div className="left">
-              <p id="headerfamilyhealth">Family health</p>
-              <p id="text1">
-                Give your family a healthy gift in this covid situation.
-              </p>
-            </div>
-            <img src="amico.png" className="person-with-masks" />
-          </div>
+    <div className="package-page">
+      <div class="top-container">
+        <div class="up">
+          <a href="url" id="healthpackages">
+            Health Packages &nbsp;
+          </a>
+          <i class="fas fa-chevron-right"></i>
+          <span id="familyhealthpackages"> &nbsp; Family health packages</span>
         </div>
-        <div className="mid-container">
-          <p className="question">Why Family health packages?</p>
-          <div className="boxfortext1">
-            <p id="text3">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-              adipiscing nibh ac praesent dui. Sit viverra molestie posuere
-              donec arcu. Massa neque nibh non a et sapien, et semper. Tincidunt
-              purus tellus eu orci, pulvinar aliquet ultrices commodo. Etiam
-              pellentesque lectus enim velit, tellus. Risus lectus interdum
-              faucibus lectus. Gravida elementum vitae proin elit, lacus,
-              libero. Tempor, ut non quis maecenas amet volutpat ut enim. Cursus
-              cras nec amet, sed. Ut volutpat proin at amet. Suspendisse quam
-              orci mi non ipsum, orci leo. Consectetur fringilla euismod
-              vehicula habitant egestas sed turpis mi. Gravida potenti eu
-              consequat ac. Tempus et diam, odio quis sit adipiscing maecenas eu
-              varius. Odio elit velit, tempus odio facilisis mauris a tempus.
-              Egestas quam viverra sit in odio. Arcu sapien, purus netus duis.
-              At rutrum ultrices magna magna faucibus condimentum egestas.
+
+        <div className="down">
+          <div className="fcp_header_desc">
+            <p id="headerfamilyhealth">Family health</p>
+            <p id="text1">
+              Give your family a healthy gift in this covid situation.
             </p>
           </div>
-          <p className="question">Purposes of family care packages</p>
-          <div className="boxfortext2">
-            <ul>
-              <li className="list1">
-                To provide the health, nutrition, physical and emotional care
-                and personal individual development needs of its members, as
-                well as to maintain family morale and the family’s culture’s
-                customs, values and beleifs.{" "}
-              </li>
-              <li className="list1">
-                To maintain personal medical records and keep tracks of a
-                chronic illness.
-              </li>
-              <li className="list1">
-                To enable an easy and immediate response to patient needs.
-              </li>
-              <li className="list1">
-                To provide all the necessary health services at their doorstep.
-              </li>
-            </ul>
-          </div>
-          <p className="question">Packages details</p>
+          <img src="amico.png" className="person-with-masks" />
         </div>
-        <div className="bottom-container">
-          <div className="boxes">
-            <p className="Program">Basic</p>
-            <p className="text9">
-              Our standard health care package for your family.
-            </p>
-            <p className="price">Rs. 20,000</p>
-            <p className="peryear">per year</p>
-            <ul>
-              <li className="list2">Up to 6 members.</li>
-              <li className="list2">
-                Medical consultation at home by experienced doctors(8 times)
-              </li>
-              <li className="list2">
-                Online medical consultation by expert doctors(12 times)
-              </li>
-              <li className="list2">
-                Home visit by professional nurses(8 times)
-              </li>
-              <li className="list2">
-                Follow up by customer health services department
-              </li>
-              <li className="list2">Emergency visit</li>
-              <li className="list2">
-                Nutrition/ deit plan by an experienced dietician
-              </li>
-              <li className="list2">Off on lab charges(7%)</li>
-              <li className="list2">Off on medicines(7%)</li>
-            </ul>
-            <button className="button">Get Started</button>
-          </div>
-          <div className="boxes" id="standard">
-            <p className="popular">Popular</p>
-            <p className="Program" id="stan">
-              Standard
-            </p>
-            <p className="text9">
-              Our standard health care package for your family.
-            </p>
-            <p className="price">Rs. 20,000</p>
-            <p className="peryear">per year</p>
-            <ul>
-              <li className="list2">Up to 6 members.</li>
-              <li className="list2">
-                Medical consultation at home by experienced doctors(8 times)
-              </li>
-              <li className="list2">
-                Online medical consultation by expert doctors(12 times)
-              </li>
-              <li className="list2">
-                Home visit by professional nurses(8 times)
-              </li>
-              <li className="list2">
-                Follow up by customer health services department
-              </li>
-              <li className="list2">Emergency visit</li>
-              <li className="list2">
-                Nutrition/ deit plan by an experienced dietician
-              </li>
-              <li className="list2">Off on lab charges(7%)</li>
-              <li className="list2">Off on medicines(7%)</li>
-            </ul>
-            <button className="button">Get Started</button>
-          </div>
-          <div className="boxes">
-            <p className="Program">Premium</p>
-            <p className="text9">
-              Our standard health care package for your family.
-            </p>
-            <p className="price">Rs. 20,000</p>
-            <p className="peryear">per year</p>
-            <ul>
-              <li className="list2">Up to 6 members.</li>
-              <li className="list2">
-                Medical consultation at home by experienced doctors(8 times)
-              </li>
-              <li className="list2">
-                Online medical consultation by expert doctors(12 times)
-              </li>
-              <li className="list2">
-                Home visit by professional nurses(8 times)
-              </li>
-              <li className="list2">
-                Follow up by customer health services department
-              </li>
-              <li className="list2">Emergency visit</li>
-              <li className="list2">
-                Nutrition/ deit plan by an experienced dietician
-              </li>
-              <li className="list2">Off on lab charges(7%)</li>
-              <li className="list2">Off on medicines(7%)</li>
-            </ul>
-            <button className="button">Get Started</button>
-          </div>
+      </div>
+      <div className="mid-container">
+        <p className="question">Why Family health packages?</p>
+        <div className="boxfortext1">
+          <p id="text3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            adipiscing nibh ac praesent dui. Sit viverra molestie posuere donec
+            arcu. Massa neque nibh non a et sapien, et semper. Tincidunt purus
+            tellus eu orci, pulvinar aliquet ultrices commodo. Etiam
+            pellentesque lectus enim velit, tellus. Risus lectus interdum
+            faucibus lectus. Gravida elementum vitae proin elit, lacus, libero.
+            Tempor, ut non quis maecenas amet volutpat ut enim. Cursus cras nec
+            amet, sed. Ut volutpat proin at amet. Suspendisse quam orci mi non
+            ipsum, orci leo. Consectetur fringilla euismod vehicula habitant
+            egestas sed turpis mi. Gravida potenti eu consequat ac. Tempus et
+            diam, odio quis sit adipiscing maecenas eu varius. Odio elit velit,
+            tempus odio facilisis mauris a tempus. Egestas quam viverra sit in
+            odio. Arcu sapien, purus netus duis. At rutrum ultrices magna magna
+            faucibus condimentum egestas.
+          </p>
+        </div>
+        <p className="question">Purposes of family care packages</p>
+        <div className="boxfortext2">
+          <ul>
+            <li className="list1">
+              To provide the health, nutrition, physical and emotional care and
+              personal individual development needs of its members, as well as
+              to maintain family morale and the family’s culture’s customs,
+              values and beleifs.{" "}
+            </li>
+            <li className="list1">
+              To maintain personal medical records and keep tracks of a chronic
+              illness.
+            </li>
+            <li className="list1">
+              To enable an easy and immediate response to patient needs.
+            </li>
+            <li className="list1">
+              To provide all the necessary health services at their doorstep.
+            </li>
+          </ul>
+        </div>
+        <p className="question">Packages details</p>
+      </div>
+      <div className="bottom-container">
+        <div className="boxes">
+          <p className="Program">Basic</p>
+          <p className="text9">
+            Our standard health care package for your family.
+          </p>
+          <p className="price">Rs. 20,000</p>
+          <p className="peryear">per year</p>
+          <ul>
+            <li className="list2">Up to 6 members.</li>
+            <li className="list2">
+              Medical consultation at home by experienced doctors(8 times)
+            </li>
+            <li className="list2">
+              Online medical consultation by expert doctors(12 times)
+            </li>
+            <li className="list2">
+              Home visit by professional nurses(8 times)
+            </li>
+            <li className="list2">
+              Follow up by customer health services department
+            </li>
+            <li className="list2">Emergency visit</li>
+            <li className="list2">
+              Nutrition/ deit plan by an experienced dietician
+            </li>
+            <li className="list2">Off on lab charges (7%)</li>
+            <li className="list2">Off on medicines (7%)</li>
+          </ul>
+          <button className="button">Get Started</button>
+        </div>
+        <div className="boxes" id="standard">
+          <p className="popular">Popular</p>
+          <p className="Program" id="stan">
+            Standard
+          </p>
+          <p className="text9">
+            Our standard health care package for your family.
+          </p>
+          <p className="price">Rs. 20,000</p>
+          <p className="peryear">per year</p>
+          <ul>
+            <li className="list2">Up to 6 members.</li>
+            <li className="list2">
+              Medical consultation at home by experienced doctors(8 times)
+            </li>
+            <li className="list2">
+              Online medical consultation by expert doctors(12 times)
+            </li>
+            <li className="list2">
+              Home visit by professional nurses(8 times)
+            </li>
+            <li className="list2">
+              Follow up by customer health services department
+            </li>
+            <li className="list2">Emergency visit</li>
+            <li className="list2">
+              Nutrition/ deit plan by an experienced dietician
+            </li>
+            <li className="list2">Off on lab charges (7%)</li>
+            <li className="list2">Off on medicines (7%)</li>
+          </ul>
+          <button className="button">Get Started</button>
+        </div>
+        <div className="boxes">
+          <p className="Program">Premium</p>
+          <p className="text9">
+            Our standard health care package for your family.
+          </p>
+          <p className="price">Rs. 20,000</p>
+          <p className="peryear">per year</p>
+          <ul>
+            <li className="list2">Up to 6 members.</li>
+            <li className="list2">
+              Medical consultation at home by experienced doctors(8 times)
+            </li>
+            <li className="list2">
+              Online medical consultation by expert doctors(12 times)
+            </li>
+            <li className="list2">
+              Home visit by professional nurses(8 times)
+            </li>
+            <li className="list2">
+              Follow up by customer health services department
+            </li>
+            <li className="list2">Emergency visit</li>
+            <li className="list2">
+              Nutrition/ deit plan by an experienced dietician
+            </li>
+            <li className="list2">Off on lab charges (7%)</li>
+            <li className="list2">Off on medicines (7%)</li>
+          </ul>
+          <button className="button">Get Started</button>
         </div>
       </div>
     </div>
