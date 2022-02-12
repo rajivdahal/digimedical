9b8ce505da4449c49806c4bdc9899587ab1a7f25
Rajiv Dahal <rajivdahal2@gmail.com>
2022-02-13 01:00:28 +0545

    before content change

--- src/component/dashboard/hospitaldashboard/hospitalsidebar/hospitalsidebar.component.js
+++ src/component/dashboard/hospitaldashboard/hospitalsidebar/hospitalsidebar.component.js
@@ -1,7 +1,6 @@
 import { Link,useHistory } from "react-router-dom"
 import CoPresentRounded from "@mui/icons-material/CoPresentRounded";
 import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined';
-
 const Hospitalsidebar = (props) => {
   const history=useHistory()
   console.log("props in sidebar is",props)
@@ -22,7 +21,6 @@ const Hospitalsidebar = (props) => {
               <span className="menu-title">Dashboard</span>
             </a>
           </li>
-
           <li className="nav-item">
             <a className="nav-link " data-toggle="collapse" href="#appointment" aria-expanded="false" aria-controls="appointment">
             <AssignmentIndOutlinedIcon className="dash-icon "></AssignmentIndOutlinedIcon>
