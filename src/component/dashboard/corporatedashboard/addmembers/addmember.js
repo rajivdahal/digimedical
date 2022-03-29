544e15ae79e3a25482e13b369024f91a081d4960
Bibek <bibekneupane51@gmail.com>
2022-03-29 13:47:09 +0545

    home content margin top is managed

--- src/component/dashboard/corporatedashboard/addmembers/addmember.js
+++ src/component/dashboard/corporatedashboard/addmembers/addmember.js
@@ -79,25 +79,7 @@ export default function addMember() {
                         </Field>
                       </div>
                     </Col>
-                    <Col md={4}>
-                      <div className=" form-group  select-label">
-                        <label>Blood Group : </label>
-                        <Field
-                          class="select-control profile-field"
-                          as="select"
-                          name="bloodGroup"
-                        >
-                          <option value="A+">A-postivie</option>
-                          <option value="A-">A-negative</option>
-                          <option value="B+">B-postive</option>
-                          <option value="B-">B-negative</option>
-                          <option value="O+">O-positive</option>
-                          <option value="O-">O-negative</option>
-                          <option value="AB+">AB-postive</option>
-                          <option value="AB-">AB-negative</option>
-                        </Field>
-                      </div>
-                    </Col>
+
                     <Col md={4}>
                       <div className=" form-group select-label">
                         <label>Date Of Birth : </label>
@@ -105,52 +87,6 @@ export default function addMember() {
                           name="dob"
                           className="form-control profile-field"
                           type="date"
-                        />
-                      </div>
-                    </Col>
-                    <Col md={4}>
-                      <div className=" form-group select-label">
-                        <label>Weight : </label>
-                        <Field
-                          name="weight"
-                          className="form-control profile-field"
-                        />
-                      </div>
-                    </Col>
-                    <Col md={4}>
-                      <div className=" form-group select-label">
-                        <label>Height : </label>
-                        <Field
-                          name="height"
-                          className="form-control profile-field"
-                        />
-                      </div>
-                    </Col>
-                    <Col md={4}>
-                      <div className=" form-group select-label">
-                        <label>Contact Number : </label>
-                        <Field
-                          name="contactNo"
-                          className="form-control profile-field"
-                        />
-                      </div>
-                    </Col>
-                    <Col md={6}>
-                      <div className=" form-group select-label">
-                        <label>Father's Name : </label>
-                        <Field
-                          name="fatherName"
-                          className="form-control profile-field"
-                        />
-                      </div>
-                    </Col>
-
-                    <Col md={6}>
-                      <div className=" form-group select-label">
-                        <label>Previous Diseases : </label>
-                        <Field
-                          name="previousDisease"
-                          className="form-control profile-field"
                         />
                       </div>
                     </Col>
--- src/component/dashboard/corporatedashboard/addmembers/searchmember.js
+++ src/component/dashboard/corporatedashboard/addmembers/searchmember.js
@@ -4,119 +4,121 @@ import Avatar from "../../../../assets/avatars.png";
 import "./searchmember.css";
 export default function searchMember() {
   return (
-    <Container>
-      <div className="corp-admem-search-bar">
-        <div className="cadmem-search-bar">
-          <input type="text" />
-          <div className="cadmem-search-icon">
-            {" "}
-            <span>
-              <i class="fas fa-search"></i>
-            </span>
+    <div className="corp-ad-search-mem">
+      <Container>
+        <div className="corp-admem-search-bar">
+          <div className="cadmem-search-bar">
+            <input type="text" />
+            <div className="cadmem-search-icon">
+              {" "}
+              <span>
+                <i class="fas fa-search"></i>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <Row>
-        <Col md={3}>
-          <div className="about_user_viewp">
-            <div className="image-profile textAlign-center">
-              <Image
-                src={Avatar}
-                fluid
-                roundedCircle
-                className="imag-profile"
-              ></Image>
-            </div>
-            {/* <div className="textAlign-center">
+        <Row>
+          <Col md={3}>
+            <div className="about_user_viewp">
+              <div className="image-profile textAlign-center">
+                <Image
+                  src={Avatar}
+                  fluid
+                  roundedCircle
+                  className="imag-profile"
+                ></Image>
+              </div>
+              {/* <div className="textAlign-center">
               <div className="credentials-name">
                 {props.firstname} {props.middlename} {props.lastname}
               </div>
               <div className="credentials-email">{props.email}</div>
             </div> */}
-            <div className="textAlign-center">
-              <div className="credentials-name"></div>
-              <div className="credentials-email"></div>
+              <div className="textAlign-center">
+                <div className="credentials-name"></div>
+                <div className="credentials-email"></div>
+              </div>
             </div>
-          </div>
-        </Col>
+          </Col>
 
-        <Col md={9}>
-          <Row>
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Role</span>
-                <span>:</span>
-                <span className="info-value">Patient</span>
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Address </span>
-                <span>:</span>
-                <span className="info-value">{}</span>
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">DOB</span>
-                <span>:</span>
-                <span className="info-value">{}</span>
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Age</span>
-                <span>:</span>
-                <span className="info-value">{}</span>
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Height</span>
-                <span>:</span>
-                <span className="info-value">{}</span>
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Weight</span>
-                <span>:</span>
-                <span className="info-value">{}</span>
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Blood Group</span>
-                <span>:</span>
-                <span className="info-value">{}</span>
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Gender</span>
-                <span>:</span>
-                <span className="info-value"></span>
-              </div>
-            </Col>
+          <Col md={9}>
+            <Row>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Role</span>
+                  <span>:</span>
+                  <span className="info-value">Patient</span>
+                </div>
+              </Col>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Address </span>
+                  <span>:</span>
+                  <span className="info-value">{}</span>
+                </div>
+              </Col>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">DOB</span>
+                  <span>:</span>
+                  <span className="info-value">{}</span>
+                </div>
+              </Col>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Age</span>
+                  <span>:</span>
+                  <span className="info-value">{}</span>
+                </div>
+              </Col>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Height</span>
+                  <span>:</span>
+                  <span className="info-value">{}</span>
+                </div>
+              </Col>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Weight</span>
+                  <span>:</span>
+                  <span className="info-value">{}</span>
+                </div>
+              </Col>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Blood Group</span>
+                  <span>:</span>
+                  <span className="info-value">{}</span>
+                </div>
+              </Col>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Gender</span>
+                  <span>:</span>
+                  <span className="info-value"></span>
+                </div>
+              </Col>
 
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Contact</span>
-                <span>:</span>
-                <span className="info-value"></span>
-              </div>
-            </Col>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Contact</span>
+                  <span>:</span>
+                  <span className="info-value"></span>
+                </div>
+              </Col>
 
-            <Col md={6}>
-              <div className="info-block">
-                <span className="info-label">Father's Name</span>
-                <span>:</span>
-                <span className="info-value">Prem Gautam Ale</span>
-              </div>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+              <Col md={6}>
+                <div className="info-block">
+                  <span className="info-label">Father's Name</span>
+                  <span>:</span>
+                  <span className="info-value">Prem Gautam Ale</span>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
